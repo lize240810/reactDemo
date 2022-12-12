b8f2f906621e9c067c9d743c604641dbb3be1c27
lize <lize240810@163.com>
2022-12-12 21:53:46 +0800

    胜利后停止继续前进

--- demo03/src/component/Board.tsx
+++ demo03/src/component/Board.tsx
@@ -1,6 +1,27 @@
 import React from "react";
 import Square from "./Square";
 
+function calculateWinner(squares: []) {
+    // 9种能胜利的索引
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
 /**
  * 类组件
  */
@@ -13,14 +34,18 @@ class Board extends React.Component {
         };
     }
 
+
+
     handleClick(i: any) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
 
         squares[i] = this.state.xIsNext ? '⭕️':'❌'
         this.setState({squares: squares,
             xIsNext: !this.state.xIsNext,
         });
-
     }
 
     renderSquare(i: any) {
@@ -28,7 +53,14 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? '️⭕':'❌');
+        const winner = calculateWinner(this.state.squares);
+        let status: string;
+
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? '❌' : '️⭕');
+        }
 
         return (
             <div>
